e8721627503d1e227d46de8c7e91a67d50ec6434
gadgelogger <sarayou273142910@gmail.com>
2024-01-29 17:43:40 +0900

    add:Add WeatherWidget component

--- lib/Weather/weatger_top.dart
+++ lib/Weather/weatger_top.dart
@@ -1,14 +1,8 @@
-//todo:再利用可能なコンポーネントを分ける
-//))などの間にカンマを入れるように警告を出すLintをつける
-//通信処理と画面のUIを分ける
-//Freezedを使ってみる
-//fontSize: 20.sp, //themedataを使えば書き方が簡単になるので調べてみる
-//weatherData['main']['temp_max'] -  273.15
-//気温を計算する部分を関数にしてあげる
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:http/http.dart' as http; //分ける
+import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:ous/apikey.dart';
 
@@ -20,7 +14,7 @@ class Weather extends StatefulWidget {
 }
 
 class _WeatherState extends State<Weather> {
-  static dynamic weatherData;
+  var weatherData;
   List<dynamic> forecast = [];
 
   @override
@@ -70,7 +64,8 @@ class _WeatherState extends State<Weather> {
         body: weatherData != null
             ? SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                child: Container(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
@@ -89,7 +84,7 @@ class _WeatherState extends State<Weather> {
                             DateTime.fromMillisecondsSinceEpoch(
                                 weatherData['dt'] * 1000)),
                         style: TextStyle(
-                          fontSize: 20.sp, //themedataを調べてみる
+                          fontSize: 20.sp,
                         ),
                       ),
                       Row(
@@ -106,7 +101,7 @@ class _WeatherState extends State<Weather> {
                                     .toStringAsFixed(0) +
                                 "°",
                             style: TextStyle(
-                                fontSize: 80.sp, //themedataを調べてみる
+                                fontSize: 80.sp,
                                 color: Theme.of(context).brightness ==
                                         Brightness.dark
                                     ? Colors.white
@@ -115,8 +110,7 @@ class _WeatherState extends State<Weather> {
                         ],
                       ),
                       Text(
-                        (weatherData['main']['temp_max'] -
-                                    273.15) //todo別の変数で分けてあげる
+                        (weatherData['main']['temp_max'] - 273.15)
                                 .toStringAsFixed(0) +
                             "°" +
                             "/" +
@@ -314,7 +308,7 @@ class _WeatherState extends State<Weather> {
                     ],
                   ),
                 ),
-              )
+              ))
             : const Center(
                 child: CircularProgressIndicator(),
               ),
@@ -329,7 +323,7 @@ class Imabari extends StatefulWidget {
 }
 
 class _ImabariState extends State<Imabari> {
-  static dynamic weatherData;
+  var weatherData;
   List<dynamic> forecast = [];
 
   @override
@@ -379,172 +373,169 @@ class _ImabariState extends State<Imabari> {
         body: weatherData != null
             ? SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.location_on_outlined),
-                          Text('今治')
-                        ],
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Icon(Icons.location_on_outlined), Text('今治')],
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Text(
+                      DateFormat('M/d(E)HH:mm').format(
+                          DateTime.fromMillisecondsSinceEpoch(
+                              weatherData['dt'] * 1000)),
+                      style: TextStyle(
+                        fontSize: 20.sp,
                       ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Text(
-                        DateFormat('M/d(E)HH:mm').format(
-                            DateTime.fromMillisecondsSinceEpoch(
-                                weatherData['dt'] * 1000)),
-                        style: TextStyle(
-                          fontSize: 20.sp,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 130.h,
+                          width: 150.w,
+                          child: Image.network(
+                              "http://openweathermap.org/img/wn/${weatherData['weather'][0]['icon']}@4x.png"),
                         ),
+                        Text(
+                          (weatherData['main']['temp'] - 273.15)
+                                  .toStringAsFixed(0) +
+                              "°",
+                          style: TextStyle(
+                              fontSize: 80.sp,
+                              color: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.black),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      (weatherData['main']['temp_max'] - 273.15)
+                              .toStringAsFixed(0) +
+                          "°" +
+                          "/" +
+                          (weatherData['main']['temp_min'] - 273.15)
+                              .toStringAsFixed(0) +
+                          "°",
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Text(
+                      weatherData['weather'][0]['description'],
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
                         children: [
+                          Column(
+                            children: [
+                              Text(
+                                '降水量',
+                                style: TextStyle(fontSize: 15.sp),
+                              ),
+                              Text(
+                                weatherData['rain'] != null
+                                    ? "${weatherData['rain']['1h']}mm"
+                                    : "0mm",
+                                style: TextStyle(
+                                  fontSize: 30.sp,
+                                ),
+                              ),
+                            ],
+                          ),
                           SizedBox(
-                            height: 130.h,
-                            width: 150.w,
-                            child: Image.network(
-                                "http://openweathermap.org/img/wn/${weatherData['weather'][0]['icon']}@4x.png"),
+                              height: 50.h, child: const VerticalDivider()),
+                          Column(
+                            children: [
+                              Text(
+                                '湿度',
+                                style: TextStyle(fontSize: 15.sp),
+                              ),
+                              Text(
+                                "${weatherData['main']['humidity']}%",
+                                style: TextStyle(
+                                  fontSize: 30.sp,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            (weatherData['main']['temp'] - 273.15)
-                                    .toStringAsFixed(0) +
-                                "°",
-                            style: TextStyle(
-                                fontSize: 80.sp,
-                                color: Theme.of(context).brightness ==
-                                        Brightness.dark
-                                    ? Colors.white
-                                    : Colors.black),
+                          SizedBox(
+                              height: 50.h, child: const VerticalDivider()),
+                          Column(
+                            children: [
+                              Text(
+                                '風速',
+                                style: TextStyle(fontSize: 15.sp),
+                              ),
+                              Text(
+                                "${weatherData['wind']['speed']}m/s",
+                                style: TextStyle(
+                                  fontSize: 30.sp,
+                                ),
+                              ),
+                            ],
                           ),
+                          SizedBox(
+                              height: 50.h, child: const VerticalDivider()),
+                          Column(
+                            children: [
+                              Text(
+                                '気圧',
+                                style: TextStyle(fontSize: 15.sp),
+                              ),
+                              Text(
+                                "${weatherData['main']['pressure']}hPa",
+                                style: TextStyle(
+                                  fontSize: 30.sp,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                              height: 50.h, child: const VerticalDivider()),
+                          Column(
+                            children: [
+                              Text(
+                                '体感温度',
+                                style: TextStyle(fontSize: 15.sp),
+                              ),
+                              Text(
+                                (weatherData['main']['feels_like'] - 273.15)
+                                        .toStringAsFixed(0) +
+                                    "°",
+                                style: TextStyle(
+                                  fontSize: 30.sp,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                              height: 50.h, child: const VerticalDivider()),
                         ],
                       ),
-                      Text(
-                        (weatherData['main']['temp_max'] - 273.15)
-                                .toStringAsFixed(0) +
-                            "°" +
-                            "/" +
-                            (weatherData['main']['temp_min'] - 273.15)
-                                .toStringAsFixed(0) +
-                            "°",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          color: Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      Text(
-                        weatherData['weather'][0]['description'],
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  '降水量',
-                                  style: TextStyle(fontSize: 15.sp),
-                                ),
-                                Text(
-                                  weatherData['rain'] != null
-                                      ? "${weatherData['rain']['1h']}mm"
-                                      : "0mm",
-                                  style: TextStyle(
-                                    fontSize: 30.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                                height: 50.h, child: const VerticalDivider()),
-                            Column(
-                              children: [
-                                Text(
-                                  '湿度',
-                                  style: TextStyle(fontSize: 15.sp),
-                                ),
-                                Text(
-                                  "${weatherData['main']['humidity']}%",
-                                  style: TextStyle(
-                                    fontSize: 30.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                                height: 50.h, child: const VerticalDivider()),
-                            Column(
-                              children: [
-                                Text(
-                                  '風速',
-                                  style: TextStyle(fontSize: 15.sp),
-                                ),
-                                Text(
-                                  "${weatherData['wind']['speed']}m/s",
-                                  style: TextStyle(
-                                    fontSize: 30.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                                height: 50.h, child: const VerticalDivider()),
-                            Column(
-                              children: [
-                                Text(
-                                  '気圧',
-                                  style: TextStyle(fontSize: 15.sp),
-                                ),
-                                Text(
-                                  "${weatherData['main']['pressure']}hPa",
-                                  style: TextStyle(
-                                    fontSize: 30.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                                height: 50.h, child: const VerticalDivider()),
-                            Column(
-                              children: [
-                                Text(
-                                  '体感温度',
-                                  style: TextStyle(fontSize: 15.sp),
-                                ),
-                                Text(
-                                  (weatherData['main']['feels_like'] - 273.15)
-                                          .toStringAsFixed(0) +
-                                      "°",
-                                  style: TextStyle(
-                                    fontSize: 30.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                                height: 50.h, child: const VerticalDivider()),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      Container(
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
                         decoration: BoxDecoration(
                           color: Theme.of(context).colorScheme.primary,
                           borderRadius: BorderRadius.circular(10),
@@ -617,12 +608,10 @@ class _ImabariState extends State<Imabari> {
                               );
                             },
                           ),
-                        ),
-                      )
-                    ],
-                  ),
+                        ))
+                  ],
                 ),
-              )
+              ))
             : const Center(
                 child: CircularProgressIndicator(),
               ),
