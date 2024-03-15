27adea7a50360a65b9e1bf52b5419edb2843f072
gadgelogger <sarayou273142910@gmail.com>
2024-03-15 20:56:30 +0900

    fix:メンテナンス機能を削除

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:ous/api/service/remote_config_service.dart';
 import 'package:ous/screens/account/login_screen.dart';
 import 'package:ous/screens/main_screen.dart';
-import 'package:ous/screens/maintenance_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,32 +11,18 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashScreenState extends State<SplashScreen> {
-  final RemoteConfigService _remoteConfigService = RemoteConfigService();
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _remoteConfigService.checkMaintenance(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, authSnapshot) {
+        if (authSnapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         }
-        if (snapshot.data == true) {
-          return const MaintenanceScreen();
+        if (authSnapshot.hasData) {
+          return MainScreen();
         } else {
-          return StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, authSnapshot) {
-              if (authSnapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              if (authSnapshot.hasData) {
-                return MainScreen();
-              } else {
-                return const Login();
-              }
-            },
-          );
+          return const Login();
         }
       },
     );
