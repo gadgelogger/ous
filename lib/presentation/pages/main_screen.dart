a8706b526541f268a892bbaae9dd867c5f88f36a
gadgelogger <sarayou273142910@gmail.com>
2024-07-24 12:48:27 +0900

    featBottomNavigationBarからNavigationBarに変更

--- lib/presentation/pages/main_screen.dart
+++ lib/presentation/pages/main_screen.dart
@@ -1,9 +1,8 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 // Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 // Project imports:
 import 'package:ous/presentation/pages/home/home_screen.dart';
 import 'package:ous/presentation/pages/info/info_screen.dart';
@@ -12,35 +11,37 @@ import 'package:ous/presentation/pages/review/review_screen.dart';
 final baseTabViewProvider = StateProvider<ViewType>((ref) => ViewType.home);
 
 class MainScreen extends ConsumerWidget {
-  final widgets = [
-    const Home(),
-    const Info(),
-    const ReviewTopScreen(),
+  const MainScreen({Key? key}) : super(key: key);
+
+  final List<Widget> _widgets = const [
+    Home(),
+    Info(),
+    ReviewTopScreen(),
   ];
-  MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewState = ref.watch(baseTabViewProvider);
     return Scaffold(
       body: Center(
-        child: widgets[viewState.index],
+        child: _widgets[viewState.index],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: viewState.index,
-        onTap: (int index) {
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: viewState.index,
+        onDestinationSelected: (int index) {
+          HapticFeedback.mediumImpact();
           ref.read(baseTabViewProvider.notifier).state = ViewType.values[index];
         },
-        items: const [
-          BottomNavigationBarItem(
+        destinations: const [
+          NavigationDestination(
             icon: Icon(Icons.home_outlined),
             label: 'ホーム',
           ),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: Icon(Icons.info_outline),
             label: 'お知らせ',
           ),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: Icon(Icons.school_outlined),
             label: '講義評価',
           ),
